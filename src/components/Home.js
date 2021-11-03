2edd49d3d6cce8bd2ac94f7eb6756723606396d3
vinson <dagger1245653@gmail.com>
2021-11-03 15:37:24 -0700

    Create basic homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <div id="homePage">
+    	<h1 id="welcome">Welcome to Vinson Catalogue!</h1>
+        <div id="biography">
+          <p>Made by the humble Donald Eriksson in 1955, the Vinson Catalogue
+						Has been the leading provider in the online shopping industry for decades now.
+						And now for the past 5 years, we are now trying to expand out business and ship anything from fresh produce to
+						the newest tech products in the same day. The Vinson Catalogue caters to both consumer and niche markets, meaning
+						that you will be guaranteed to be able to find something that you are looking for, whether it's as obscure as seeing an eagle perched on a cactus
+						pear tree eating a snake. Vinson Catalogue also takes immense pride in the quality of the items that we sell, and it is guaranteed to be
+						in mint condition or else we will refund your money immediately. Vinson Catalogue is proud to be one of the best online shopping services for the past 7 decades, and
+						we hope that you will enjoy our services just as much as we enjoy shopping with you.
+				</p>
+				<p id="signatureHead">Signed</p>
+				<p id="signature">Gallium French Amadela Eriksson</p>
+      </div>
+		</div>
   );
 };
 
