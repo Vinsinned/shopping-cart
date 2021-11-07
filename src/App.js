83ebaaf1778624b7bda27fcae8a566ae54d802a8
vinson <dagger1245653@gmail.com>
2021-11-07 14:46:13 -0800

    Create basic about us page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   //this will be my about
   return (
-    <div>
-      <h1>About us</h1>
+    <div id="aboutUs">
+      <div id="header">
+        <h1>VINSON CATALOGUE</h1>
+        <h2>THE NUMBER ONE LUXURY COMPANY</h2>
+      </div>
+      <img src="https://t4.ftcdn.net/jpg/04/06/91/91/360_F_406919161_J0pGxe1sewqnk5dnvyRS77MKmEd6SVac.jpg" alt=""/>
     </div>
   );
 }
